72b780a06bbe751058a20cf616eacb476ec90342
malloryrbush <malloryrbush@hotmail.com>
2018-07-11 13:25:49 -0400

    initial commit

---
+++ indexJavaScript.js
@@ -0,0 +1,13 @@
+﻿function Replace() {
+    var x = document.getElementById("reg");
+    var y = document.getElementById("neon");
+
+    if (x.style.display == "none") {
+        x.style.display = "";
+        y.style.display = "none";
+    }
+    else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
